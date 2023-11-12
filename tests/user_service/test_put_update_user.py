ffba89a6ff21063c3b6a05cd36471a8fe693dc6e
olga <olgamursina@yandex.ru>
2023-11-12 14:00:05 +0100

    update user test

--- tests/user_service/test_put_update_user.py
+++ tests/user_service/test_put_update_user.py
@@ -34,71 +34,68 @@ def test_update_user(create_and_login_user, generate_email, generate_name):
     assert updated_user_body["customer_type"] == user_body["customer_type"]
 
 
-def test_update_non_existent_user(create_test_users_body, generate_email, generate_name):
-    r = UserService().post_user(data=create_test_users_body)
+def test_update_non_existence_user(create_and_login_user, generate_email, generate_name):
+    headers = {
+        'email': create_and_login_user[0]['email'],
+        'x-auth-token': create_and_login_user[1]
+    }
 
-    assert r.status_code == 201
+    user_body = deepcopy(create_and_login_user[0])
+    user_body["name"] = generate_name
+    user_body["company_type"] = "private"
 
-    create_test_users_body["name"] = generate_name
-    create_test_users_body["email"] = generate_email
-    create_test_users_body["customer_type"] = "private"
-
-    r = UserService().update_user(data=create_test_users_body, user_id=12345)
+    r = UserService().update_user(data=user_body, user_id=1111111, headers=headers)
 
     assert r.status_code == 400
     assert r.json() == Errors.user_not_found
 
 
-def test_update_user_long_date_birth(create_test_users_body, generate_email, generate_name):
-    r = UserService().post_user(data=create_test_users_body)
+def test_update_user_long_date_birth(create_and_login_user, create_test_users_body, generate_email, generate_name):
+    headers = {
+        'email': create_and_login_user[0]['email'],
+        'x-auth-token': create_and_login_user[1]
+    }
 
-    assert r.status_code == 201
+    user_id = create_and_login_user[0]['id']
 
-    user_id = r.json()['result']['id']
-    create_test_users_body["name"] = generate_name
-    create_test_users_body["email"] = generate_email
-    create_test_users_body["customer_type"] = "private"
-    create_test_users_body["date_birth"] = "1998.5.23456"
+    new_user_data = create_test_users_body
+    new_user_data["name"] = generate_name
+    new_user_data["email"] = generate_email
+    new_user_data["customer_type"] = "private"
+    new_user_data["date_birth"] = "1998.5.23456"
 
-    r = UserService().update_user(data=create_test_users_body, user_id=user_id)
+    r = UserService().update_user(data=new_user_data, user_id=user_id, headers=headers)
 
     assert r.status_code == 400
+    print(r.json())
     assert r.json() == Errors.incorrect_fields
 
 
-'''def test_update_user_duplicate_email(create_test_users_body, generate_name):
-    r = UserService().post_user(data=create_test_users_body)
+def test_update_user_duplicate_email(create_and_login_user, generate_email):
+    headers = {
+        'email': create_and_login_user[0]['email'],
+        'x-auth-token': create_and_login_user[1]
+    }
 
-    assert r.status_code == 201
+    user_body = deepcopy(create_and_login_user[0])
+    user_body["email"] = generate_email
+    user_body["company_type"] = "private"
 
-    create_test_users_body_new = deepcopy(create_test_users_body)
-    create_test_users_body_new["email"] = "test_user512@gmail.com"
-    create_test_users_body_new["name"] = generate_name
-    create_test_users_body_new["customer_type"] = "private"
-    r1 = UserService().post_user(data=create_test_users_body_new)
-    assert r1.status_code == 201
-    user_id_new = r1.json()['result']['id']
-    r2 = UserService().update_user(data=create_test_users_body, user_id=user_id_new)
+    r = UserService().update_user(data=user_body, user_id=create_and_login_user[0]["id"], headers=headers)
 
-    assert r.status_code == 400'''
+    assert r.status_code == 200
 
+    create_test_users_body_new = deepcopy(create_and_login_user)
+    create_test_users_body_new[0]["email"] = _generate_email()
+    create_test_users_body_new[0]["name"] = _generate_name()
+    create_test_users_body_new[0]["customer_type"] = "private"
 
-def test_update_user_duplicate_email(create_test_users_body):
-
-
-    r = UserService().post_user(data=create_test_users_body)
-
-    assert r.status_code == 201
-
-    create_test_users_body_new = deepcopy(create_test_users_body)
-    create_test_users_body_new["email"] = _generate_email()
-    create_test_users_body_new["name"] = _generate_name()
-    create_test_users_body_new["customer_type"] = "private"
     r1 = UserService().post_user(data=create_test_users_body_new)
 
     assert r1.status_code == 201
+    print(r1.json())
 
     user_id_new = r1.json()['result']['id']
-    r2 = UserService().update_user(data=create_test_users_body, user_id=user_id_new)
+    r2 = UserService().update_user(data=create_and_login_user, user_id=user_id_new)
 
     assert r2.status_code == 400
