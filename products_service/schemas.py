6fb1ee93dbb2c6d334b4d92fe954f61321a9814f
olga <olgamursina@yandex.ru>
2023-12-02 19:41:55 +0100

    schema creation

--- products_service/schemas.py
+++ products_service/schemas.py
@@ -10,3 +10,20 @@ class GoodsSchemas:
         "required": ["name", "price", "quantity", "description"],
         "additionalProperties": False
     }
+
+
+class BasketSchemas:
+    basket = {
+         "type": "object",
+        "properties": {
+            "id": {"type": "string"},
+            "basket_id": {"type": "string"},
+            "user_id": {"type": "string"},
+            "product_id": {"type": "string"},
+            "quantity": {"type": "integer"},
+            "sum": {"type": "integer"}
+
+        },
+        "required": ["user_id", "product_id", "quantity", "sum"],
+        "additionalProperties": False
+    }
